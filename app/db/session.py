dabf18958fae3f664b462b191c7dcaff9ef52709
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-13 23:15:39 +0700

    feat: init sql to sqlmodel

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,17 @@
-from typing import Generator
-from app.db.base import SessionLocal
+from sqlmodel import SQLModel, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession, AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-async def get_db() -> Generator:
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}/{settings.POSTGRES_DB}"
+
+engine = AsyncEngine(create_engine(DATABASE_URL, echo=True, future=True))
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
