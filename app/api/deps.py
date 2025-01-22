a207ad1ce3c401dee9d0b94a6d684c1cd9d07d8e
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-22 23:25:23 +0700

    refactor: remove unused import

--- app/api/deps.py
+++ app/api/deps.py
@@ -5,12 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import async_session_factory
 
-# def get_db() -> Generator[Session, None, None]:
-#     with Session(engine) as session:
-#         yield session
-
-# SessionDep = Annotated[Session, Depends(get_db)]
-
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_factory() as session:
         try:
