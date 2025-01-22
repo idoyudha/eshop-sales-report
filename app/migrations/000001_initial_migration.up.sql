1bf879afe3c2ea1d2fc4cde7137288c4aa400138
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-22 17:19:22 +0700

    feat: update init migration sql timestamp with timezone

--- app/migrations/000001_initial_migration.up.sql
+++ app/migrations/000001_initial_migration.up.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS "sales" (
-  "id" uuid PRIMARY KEY,
-  "user_id" uuid NOT NULL,
-  "order_id" uuid NOT NULL,
-  "product_id" uuid NOT NULL,
-  "product_quantity" integer NOT NULL,
-  "margin_per_product" float NOT NULL,
-  "created_at" timestamp NOT NULL
+CREATE TABLE sales (
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  order_id UUID NOT NULL,
+  product_id UUID NOT NULL,
+  product_quantity INTEGER NOT NULL,
+  margin_per_product DOUBLE PRECISION NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_user_id ON "sales" ("user_id");
-CREATE INDEX idx_order_id ON "sales" ("order_id");
-CREATE INDEX idx_product_id ON "sales" ("product_id");
+CREATE INDEX sales_user_id_idx ON sales(user_id);
+CREATE INDEX sales_order_id_idx ON sales(order_id);
+CREATE INDEX sales_product_id_idx ON sales(product_id);
