e37e9fb33e0279323de9fc1a44cc54c41dcba1cf
idoyudha <idowidya.yudhatama@gmail.com>
2025-02-05 11:14:54 +0700

    feat: add xlsx report

--- app/api/routes/sale.py
+++ app/api/routes/sale.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 from typing import Any
 
 from app.api.deps import AsyncSessionDep
 from app.models.sale import SalesPublic
-from app.service.sale import get_sales_service, count_total_sales_service
+from app.service.sale import get_sales_service, count_total_sales_service, generate_sales_report_xlsx_service
 
 router = APIRouter(prefix="/sales", tags=["sales"])
 
@@ -25,3 +26,15 @@ async def read_sales(
         count=count,
         message="success get"
     )
+
+@router.get("/xlsx-report")
+async def generate_sales_report_xlsx(session: AsyncSessionDep) -> Any:
+    """
+    Generate sales report in xlsx format.
+    """
+    output = await generate_sales_report_xlsx_service(session=session)
+    headers = {
+        "Content-Disposition": f"attachment; filename=sales-report.xlsx",
+    }
+    media_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    return StreamingResponse(output, media_type=media_type, headers=headers)
---
+++ app/utils/generate_xlsx.py
@@ -0,0 +1,36 @@
+import xlsxwriter
+from io import BytesIO
+from app.models.sale import SalePublic
+
+def generate_xlsx(sales: list[SalePublic]) -> BytesIO:
+    # docs: https://xlsxwriter.readthedocs.io/
+    # create an nex excel file and add worksheet
+    output = BytesIO()
+    workbook = xlsxwriter.Workbook(output, {"remove_timezone": True})
+    worksheet = workbook.add_worksheet()
+
+    # start from the first cell. rows and columns are zero indexed.
+    row, col = 0, 0
+
+    # write headers
+    headers = ["no", "user_id", "order_id", "product_id", "quantity", "margin_per_product", "created_at"]
+    for header in headers:
+        worksheet.write(row, col, header)
+        col += 1
+
+    row, col = 1, 0
+    # write data
+    for sale in sales:
+        worksheet.write(row, col, row)
+        worksheet.write(row, col + 1, str(sale.user_id))
+        worksheet.write(row, col + 2, str(sale.order_id))
+        worksheet.write(row, col + 3, str(sale.product_id))
+        worksheet.write(row, col + 4, sale.product_quantity)
+        worksheet.write(row, col + 5, sale.margin_per_product)
+        worksheet.write(row, col + 6, sale.created_at)
+        row += 1
+
+    workbook.close()
+    output.seek(0)
+    return output
+
